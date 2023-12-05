ece2b770e8f378b9c78bb6f051b052b4dd17e2f5
aluno <aluno@ifpe>
2023-12-05 15:48:42 -0300

    primeiros arquivos

---
+++ README.md
@@ -0,0 +1,1 @@
+# oxee-lixo api
---
+++ src/main/java/br/com/ifpe/oxeelixo/OxeelixoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxeelixo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxeelixoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxeelixoApiApplication.class, args);
+	}
+
+}
