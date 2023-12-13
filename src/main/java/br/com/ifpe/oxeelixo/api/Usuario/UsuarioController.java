8ccd22aa59a3097a713e13f2880c436033559d7f
mary0077 <marianagomes201@hotmail.com>
2023-12-13 14:42:14 -0300

    Documentando a API(Incompleto)

--- src/main/java/br/com/ifpe/oxeelixo/api/Usuario/UsuarioController.java
+++ src/main/java/br/com/ifpe/oxeelixo/api/Usuario/UsuarioController.java
@@ -14,9 +14,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-/*import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;*/
+import io.swagger.annotations.ApiResponses;
 
 import br.com.ifpe.oxeelixo.modelo.usuario.Usuario;
 import br.com.ifpe.oxeelixo.modelo.usuario.UsuarioService;
@@ -30,8 +30,7 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    // @ApiOperation(value = "Serviço responsável por salvar um cliente no
-    // sistema.")
+    @ApiOperation(value = "Serviço responsável por salvar um usuário no sistema.")
     @PostMapping
     public ResponseEntity<Usuario> save(@RequestBody @Valid UsuarioRequest request) {
 
@@ -39,33 +38,32 @@ public class UsuarioController {
         return new ResponseEntity<Usuario>(usuario, HttpStatus.CREATED);
     }
 
-    // @ApiOperation(value = "Serviço responsável por listar todos os usuários do
-    // sistema.")
+    @ApiOperation(value = "Serviço responsável por listar todos os usuários do sistema.")
     @GetMapping
     public List<Usuario> findAll() {
 
         return usuarioService.findAll();
     }
 
-    /*
-     * @ApiOperation(value =
-     * "Serviço responsável por obter um cliente referente ao Id passado na URL.")
-     * 
-     * @ApiResponses(value = {
-     * 
-     * @ApiResponse(code = 200, message = "Retorna  o cliente."),
-     * 
-     * @ApiResponse(code = 401, message = "Acesso não autorizado."),
-     * 
-     * @ApiResponse(code = 403, message =
-     * "Você não tem permissão para acessar este recurso."),
-     * 
-     * @ApiResponse(code = 404, message =
-     * "Não foi encontrado um registro para o Id informado."),
-     * 
-     * @ApiResponse(code = 500, message = "Foi gerado um erro no servidor."),
-     * })
-     */
+    
+      @ApiOperation(value =
+      "Serviço responsável por obter um usuário referente ao Id passado na URL.")
+      
+      @ApiResponses(value = {
+      
+      @ApiResponse(code = 200, message = "Retorna  o usuário."),
+      
+      @ApiResponse(code = 401, message = "Acesso não autorizado."),
+      
+      @ApiResponse(code = 403, message =
+      "Você não tem permissão para acessar este recurso."),
+      
+      @ApiResponse(code = 404, message =
+      "Não foi encontrado um registro para o Id informado."),
+      
+      @ApiResponse(code = 500, message = "Foi gerado um erro no servidor."),
+      })
+     
 
     @GetMapping("/{id}")
     public Usuario findById(@PathVariable Long id) {
